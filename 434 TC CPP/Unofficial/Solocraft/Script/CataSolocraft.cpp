f12e91811a0cf7c5b0f922d678ed81ce901ea862
acidmanifesto <joshua.lee.betts@gmail.com>
2021-06-02 14:05:00 -0400

    Solocraft Scripts Updated
    This resolves a Stacking of scaling when going from one instances straight to another with out exiting a instance.

--- 434 TC CPP/Unofficial/Solocraft/Script/CataSolocraft.cpp
+++ 434 TC CPP/Unofficial/Solocraft/Script/CataSolocraft.cpp
@@ -5,6 +5,7 @@ Which is a fork of https://github.com/conan513/mod-solocraft
 Refactored to Trinitycore 434
 and improved upon by Single Player Project Developer MDic
 */
+
 #include <map>
 #include "Config.h"
 #include "DatabaseEnv.h"
@@ -306,8 +307,19 @@ public:
         {
             if (SoloCraftAnnounceModule)
             {
-                ChatHandler(Player->GetSession()).SendSysMessage("This server is running the |cff4CFF00SPP CATA SoloCraft Custom |rmodule.");
+                ChatHandler(Player->GetSession()).SendSysMessage("This server is running the |cff4CFF00SPP Cata SoloCraft Custom |rmodule.");
             }
+        }
+    }
+
+    void OnLogout(Player* player) override
+    {
+        //Database query to see if an entry is still there
+        QueryResult result = CharacterDatabase.PQuery("SELECT `GUID` FROM `custom_solocraft_character_stats` WHERE GUID = %u", player->GetGUID());
+        if (result)
+        {
+            //Remove database entry as the player has logged out
+            CharacterDatabase.PExecute("DELETE FROM custom_solocraft_character_stats WHERE GUID = %u", player->GetGUID());
         }
     }
 };
@@ -324,6 +336,7 @@ public:
             ApplyBuffs(player, map, difficulty, dunLevel, numInGroup);
         }
     }
+
 private:
     std::map<uint32, float> _unitDifficulty;
     // Set the instance difficulty
@@ -417,9 +430,17 @@ private:
                     difficulty = difficulty / numInGroup;
                     difficulty = roundf(difficulty * 100) / 100; //Float variables suck - two decimal rounding
                 }
+
+                //Check Database for a current dungeon entry
+                QueryResult result = CharacterDatabase.PQuery("SELECT `GUID`, `Difficulty`, `GroupSize`, `SpellPower`, `Stats` FROM `custom_solocraft_character_stats` WHERE GUID = %u", player->GetGUID());
+
                 //Modify Player Stats
                 for (int32 i = STAT_STRENGTH; i < MAX_STATS; ++i) //STATS defined/enum in SharedDefines.h
                 {
+                    if (result)
+                    {
+                        player->HandleStatModifier(UnitMods(UNIT_MOD_STAT_START + i), TOTAL_VALUE, (*result)[1].GetFloat() * (*result)[4].GetFloat(), false);
+                    }
                     // Buff the player
                     player->HandleStatModifier(UnitMods(UNIT_MOD_STAT_START + i), TOTAL_VALUE, difficulty * SoloCraftStatsMult, true); //Unitmods enum UNIT_MOD_STAT_START defined in Unit.h line 391
                 }
@@ -430,6 +451,14 @@ private:
                 {
                     // Buff the player's mana
                     player->SetPower(POWER_MANA, player->GetMaxPower(POWER_MANA));
+
+                    //Check for Dungeon to Dungeon Transfer and remove old Spellpower buff
+                    if (result)
+                    {
+                        // remove spellpower bonus
+                        player->ApplySpellPowerBonus((*result)[3].GetUInt32() * (*result)[4].GetFloat(),false);
+                    }
+
                     //Buff Spellpower
                     if (difficulty > 0) //Debuffed characters do not get spellpower
                     {
@@ -459,11 +488,16 @@ private:
                 // Announce to player - Over Max Level Threshold
                 ss << "|cffFF0000[SoloCraft] |cffFF8000" << player->GetName() << " entered %s  - |cffFF0000You have not been buffed. |cffFF8000 Your level is higher than the max level (%i) threshold for this dungeon.";
                 ChatHandler(player->GetSession()).PSendSysMessage(ss.str().c_str(), map->GetMapName(), dunLevel + SolocraftLevelDiff);
+                ClearBuffs(player, map); //Check to revert player back to normal
             }
+
         }
         else
+        {
             ClearBuffs(player, map); //Check to revert player back to normal - Moving this here fixed logout and login while in instance buff and debuff issues
+        }
     }
+
     // Get the current group members GUIDS and return the total sum of the difficulty offset by all group members currently in the dungeon
     float GetGroupDifficulty(Player* player) {
         float GroupDifficulty = 0.0;
@@ -492,6 +526,7 @@ private:
         }
         return GroupDifficulty;
     }
+
     void ClearBuffs(Player* player, Map* map)
     {
         //Database query to get offset from the last instance player exited
